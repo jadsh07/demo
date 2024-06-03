333f0ffa943a044c11fdc15efac6f19fc61b8c1b
Jadshan <j.manokaran@homeit-system.de>
2024-06-03 13:25:11 +0530

    first commit

---
+++ src/test/java/homeit/demo/DemoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package homeit.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
